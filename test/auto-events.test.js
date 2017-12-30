bdcc18704ba5b6e0eb079061569909c030e9d069
gco <gco@emakina.com>
2017-12-30 16:50:01 +0100

    test: add first tests

--- test/auto-events.test.js
+++ test/auto-events.test.js
@@ -1,5 +1,4 @@
-/* global $ */
-/* eslint no-console:0 */
+/* global describe, it, expect */
 const autoEvents = require('../src/index');
 
 // autoEvents.parseMethod('delete(foo)'); /*?.*/
@@ -10,25 +9,81 @@ const autoEvents = require('../src/index');
 /** Mock node */
 
 class Node {
-	constructor() {
+	constructor(attributes) {
 		this.events = 0;
-		this.attributes = [{
-			name: 'data-ev-click',
-			value: 'delete(12)'
-		}];
+		this.attributes = attributes;
 	}
 
 	addEventListener(type, handler, bubbleing = false) {
 		this.events++;
-		console.log('addEventListener', type, typeof handler === 'function', bubbleing);
+		this[type] = handler;
+		this.bubbleing = bubbleing;
 	}
 }
 
-autoEvents.bindEvents(new Node(), {
-	init() {
-		$(this.root).find('> button.delete').autoEvents(this);
-	},
-	hide() {
-		this.root.classList.toggle('is-active');
+class Event {
+	constructor(type) {
+		this.type = type;
 	}
+}
+
+
+
+
+describe('should bind click event', () => {
+	const node = new Node([{
+		name: 'data-ev-click',
+		value: 'delete(12)'
+	}]);
+	const controller = {
+		delete(e, value) {
+			this.firedEvent = e;
+			this.deleted = value;
+		}
+	};
+	
+	autoEvents.bindEvents(node, controller);
+
+	it('set exactly one event', () => {
+		expect(node.events).toBe(1);
+	});
+	it('set the good event', () => {
+		expect(typeof node.click).toBe('function');
+	});
+	it('forward the argument from caller function', () => {
+		node.click();
+		expect(controller.deleted).toBe(12);
+	});
+	it('forward an event as first argument', () => {
+		const event = new Event('click');
+		node.click(event);
+		expect(controller.firedEvent).toBe(event);
+	});
 });
+
+describe('manage missing method', () => {
+	const node = new Node([{
+		name: 'data-ev-click',
+		value: 'delete()'
+	}]);
+	const controller = {
+	};
+	autoEvents.bindEvents(node, controller);
+
+	it('should throw if method not found', () => {
+		expect(() => node.click()).toThrow();
+	});
+});
+
+describe('skip non event attribute', () => {
+	const node = new Node([{
+		name: 'foo',
+		value: 'bar'
+	}]);
+	autoEvents.bindEvents(node, {});
+
+	it('do nothing', () => {
+		expect(node).toBe(node);
+	});
+});
+
