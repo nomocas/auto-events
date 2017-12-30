a6cb6542869487ca382b71f3aa9af2bd50d3b97c
gco <gco@emakina.com>
2017-12-30 16:47:09 +0100

    refactor: clean code

--- src/index.js
+++ src/index.js
@@ -16,9 +16,11 @@
 
 /* eslint curly:0, no-param-reassign:0 */
 const debug = require('debug')('autoevents');
-const functionCallParser = require('elenpi-simple-function-call-parser').default;
-const eventAttributeMatcher = /^data-ev-([\w\d-_]+)/;
-
+const functionCallParser = require('elenpi-simple-function-call-parser');
+const eventAttributeMatcher = /^data-ev-(\w+)/i;
+const autoEvents = {
+	devMode: false
+};
 function isAllowedMethod(name, ctrl) {
 	if (!ctrl[name])
 		throw new Error('method not found on component : ', name);
@@ -31,9 +33,16 @@ function checkAttribute(node, ctrl, attrib) {
 	if (matched) {
 		const eventType = matched[1];
 		debug('bind event (%s) with : %s : on : ', eventType, attrib.value, ctrl);
-		node.addEventListener(eventType, e => {
+		if (autoEvents.devMode) { // immediate parsing and check of events
 			parsed = parsed || functionCallParser.parse(attrib.value);
 			isAllowedMethod(parsed.method, ctrl);
+		}
+
+		node.addEventListener(eventType, e => {
+			if (!autoEvents.devMode) { // lazzy parsing and check of events 
+				parsed = parsed || functionCallParser.parse(attrib.value);
+				isAllowedMethod(parsed.method, ctrl);
+			}
 			debug('binded event : %s ! : ', eventType, parsed.method, parsed.arguments);
 			ctrl[parsed.method](e, ...parsed.arguments);
 		});
@@ -47,12 +56,12 @@ function bindEvents(node, ctrl) {
 		.forEach(attrib => checkAttribute(node, ctrl, attrib));
 }
 
-module.exports = {
-	bindEvents,
-	install($ = window.jQuery) {
-		$.fn.autoEvents = function autoEvents(ctrl) {
-			this.get().forEach(node => bindEvents(node, ctrl));
-			return this;
-		};
-	}
+autoEvents.bindEvents = bindEvents;
+autoEvents.install = function($ = window.jQuery) {
+	$.fn.autoEvents = function autoEvents(ctrl) {
+		this.get().forEach(node => bindEvents(node, ctrl));
+		return this;
+	};
 };
+
+module.exports = autoEvents;
